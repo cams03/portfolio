74e1a06e54c1d2bb167cd8839988fbf0930b181e
Cams03 <camille.francia@hotmail.fr>
2017-01-18 15:52:52 +0100

    div cv en cours, fleche retour ok

--- app.js
+++ app.js
@@ -4,17 +4,27 @@ $(document).ready(function () {
         $(".blocPro").fadeOut(0300);
         $(".blocCo").fadeOut(0300);
         $(".blocCV").fadeIn("slow");
+        $(".blocCV").css("display", "flex");
     })
+
     $(".middle_arrow").click(function () {
         $(".rond").fadeOut(0100);
         $(".blocCV").fadeOut(0300);
         $(".blocCo").fadeOut(0300);
         $(".blocPro").fadeIn("slow");
+        $(".blocPro").css("display", "flex");
     })
+
     $(".right_arrow").click(function () {
         $(".rond").fadeOut(0100);
         $(".blocCV").fadeOut(0300);
         $(".blocPro").fadeOut(0300);
         $(".blocCo").fadeIn("slow");
+        $(".blocCo").css("display", "flex");
+    })
+
+    $(".up_arrow").click(function () {
+        $(".blocCV").fadeOut(0100);
+        $(".rond").fadeIn(0300);
     })
 })
